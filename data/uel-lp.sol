55fa4268eed92b6880ffad8ae020b240548ccadf
Korbinian <korbinian@remeeting.com>
2022-07-10 00:14:51 +0200

    added mathprog and solution file

---
+++ data/uel-lp.sol
@@ -0,0 +1,186 @@
+Problem:    uel
+Rows:       107
+Columns:    44
+Non-zeros:  333
+Status:     OPTIMAL
+Objective:  maxOfAllErrors = 0.025 (MINimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 Eq1_2_3_down B          -1.79         -1.88               
+     2 Eq1_2_3_up   B          -1.84                        -1.8 
+     3 Eq4_down     B          -1.85          -1.9               
+     4 Eq4_up       NU          -1.9                        -1.9         < eps
+     5 Eq7_down     NL         -1.94         -1.94                   0.0833333 
+     6 Eq7_up       B          -1.99                       -1.94 
+     7 Eq8_down     B          -1.98         -2.03               
+     8 Eq8_up       NU         -2.03                       -2.03         < eps
+     9 Eq9_down     B          -2.34         -2.39               
+    10 Eq9_up       NU         -2.39                       -2.39         < eps
+    11 Eq10_down    B          -1.45          -1.5               
+    12 Eq10_up      NU          -1.5                        -1.5         < eps
+    13 Eq12_down    B          -1.57         -1.62               
+    14 Eq12_up      NU         -1.62                       -1.62     -0.166667 
+    15 Eq13_down    NL         -1.63         -1.63                        0.25 
+    16 Eq13_up      B          -1.68                       -1.63 
+    17 Eq14_down    B         -1.615         -1.66               
+    18 Eq14_up      B         -1.665                       -1.66 
+    19 Eq15_down    B          -1.78         -1.83               
+    20 Eq15_up      NU         -1.83                       -1.83    -0.0833333 
+    21 Eq16_17b_down
+                    B          -1.85         -2.18               
+    22 Eq16_17a_up  B           -1.9                       -1.85 
+    23 Eq18_down    B          -2.33         -2.38               
+    24 Eq18_up      NU         -2.38                       -2.38         < eps
+    25 Eq19_down    B         -3.035         -3.05               
+    26 Eq19_up      B         -3.085                       -3.05 
+    27 Eq20_21b_down
+                    B          -2.36         -2.52               
+    28 Eq20_21a_up  NU         -2.41                       -2.41         < eps
+    29 Eq22_down    B          -2.78         -2.83               
+    30 Eq22_up      NU         -2.83                       -2.83         < eps
+    31 Eq23_down    B          -1.61         -1.61               
+    32 Eq23_up      B          -1.66                       -1.61 
+    33 Eq24_down    B          -1.78         -1.83               
+    34 Eq24_up      NU         -1.83                       -1.83         < eps
+    35 Eq25_down    B          -1.88         -1.93               
+    36 Eq25_up      NU         -1.93                       -1.93     -0.166667 
+    37 Eq26_down    B           -1.9         -1.95               
+    38 Eq26_up      NU         -1.95                       -1.95         < eps
+    39 Eq27_down    B          -1.71         -1.76               
+    40 Eq27_up      NU         -1.76                       -1.76         < eps
+    41 Eq28_down    B          -1.78         -1.83               
+    42 Eq28_up      NU         -1.83                       -1.83         < eps
+    43 Eq29_down    B           -1.9         -1.95               
+    44 Eq29_up      NU         -1.95                       -1.95         < eps
+    45 Eq31_down    NL         -1.41         -1.41                       < eps
+    46 Eq31_up      B          -1.46                       -1.41 
+    47 Eq33_down    B          -1.55         -1.57               
+    48 Eq33_up      B           -1.6                       -1.57 
+    49 Eq34_down    B          -1.69          -1.7               
+    50 Eq34_up      B          -1.74                        -1.7 
+    51 Eq35_down    NL         -1.81         -1.81                       < eps
+    52 Eq35_up      B          -1.86                       -1.81 
+    53 Eq37_down    B          -2.21         -2.23               
+    54 Eq37_up      B          -2.26                       -2.23 
+    55 Eq38_down    B          -2.04         -2.09               
+    56 Eq38_up      NU         -2.09                       -2.09         < eps
+    57 Eq39_down    B          -2.14         -2.19               
+    58 Eq39_up      NU         -2.19                       -2.19         < eps
+    59 Eq40_down    B          -2.15          -2.2               
+    60 Eq40_up      NU          -2.2                        -2.2         < eps
+    61 Eq41_down    B          -2.49         -2.54               
+    62 Eq41_up      NU         -2.54                       -2.54         < eps
+    63 Eq42_down    B          -2.28         -2.33               
+    64 Eq42_up      NU         -2.33                       -2.33         < eps
+    65 Eq43_down    NL         -1.97         -1.97                       < eps
+    66 Eq43_up      B          -2.02                       -1.97 
+    67 Eq44_down    NL         -2.04         -2.04                       < eps
+    68 Eq44_up      B          -2.09                       -2.04 
+    69 Eq45_down    B          -2.01         -2.06               
+    70 Eq45_up      NU         -2.06                       -2.06         < eps
+    71 Eq46_down    B           -2.1         -2.15               
+    72 Eq46_up      NU         -2.15                       -2.15         < eps
+    73 Eq47_down    B           -2.3         -2.35               
+    74 Eq47_up      NU         -2.35                       -2.35         < eps
+    75 Eq49_down    B         -1.965         -2.01               
+    76 Eq49_up      B         -2.015                       -2.01 
+    77 Eq50_down    NL         -2.29         -2.29                    0.166667 
+    78 Eq50_up      B          -2.34                       -2.29 
+    79 Eq51_down    B          -2.51         -2.56               
+    80 Eq51_up      NU         -2.56                       -2.56         < eps
+    81 Eq52_down    B          -2.73         -2.78               
+    82 Eq52_up      NU         -2.78                       -2.78    -0.0833333 
+    83 Eq54_down    B          -2.37         -2.42               
+    84 Eq54_up      NU         -2.42                       -2.42         < eps
+    85 Eq55_down    B          -2.46         -2.51               
+    86 Eq55_up      NU         -2.51                       -2.51         < eps
+    87 Eq56_down    B          -2.52         -2.57               
+    88 Eq56_up      NU         -2.57                       -2.57         < eps
+    89 Eq57_down    B          -2.63         -2.68               
+    90 Eq57_up      NU         -2.68                       -2.68         < eps
+    91 Eq58_down    B          -2.29         -2.34               
+    92 Eq58_up      NU         -2.34                       -2.34         < eps
+    93 Eq59_down    B          -2.41         -2.46               
+    94 Eq59_up      NU         -2.46                       -2.46         < eps
+    95 Eq60_down    B          -2.43         -2.48               
+    96 Eq60_up      NU         -2.48                       -2.48         < eps
+    97 Eq61_down    B          -2.49         -2.54               
+    98 Eq61_up      NU         -2.54                       -2.54         < eps
+    99 Eq62_down    B          -2.57         -2.62               
+   100 Eq62_up      NU         -2.62                       -2.62         < eps
+   101 Eq63_down    B          -2.59         -2.64               
+   102 Eq63_up      NU         -2.64                       -2.64         < eps
+   103 Eq64_down    B          -2.75          -2.8               
+   104 Eq64_up      NU          -2.8                        -2.8         < eps
+   105 Eq65_down    B          -2.77         -2.82               
+   106 Eq65_up      NU         -2.82                       -2.82         < eps
+   107 maxOfAllErrors
+                    B          0.025                             
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 a            B       -0.49125                             
+     2 b            B       -0.34125                             
+     3 c            B       -0.40125                             
+     4 d            B       -0.30625                             
+     5 e            B       -0.53125                             
+     6 f            B       -0.89125                             
+     7 g            B       -0.43125                             
+     8 h            B       -0.52125                             
+     9 i            B       -0.59625                             
+    10 j            B       -0.82125                             
+    11 k            B       -0.59625                             
+    12 l            B       -0.80625                             
+    13 m            B      -0.487917                             
+    14 n            B      -0.527917                             
+    15 o            B       -0.27125                             
+    16 p            B       -0.01625                             
+    17 q            B       -0.36625                             
+    18 r            B       -0.51625                             
+    19 s            B       -0.20125                             
+    20 t            B       -0.27125                             
+    21 u            B       -0.40125                             
+    22 v            B       -0.31125                             
+    23 w            B       -0.60125                             
+    24 x            B       -0.69125                             
+    25 y            B       -0.74125                             
+    26 z            B       -0.54375                             
+    27 aa           B        -1.2575                             
+    28 bb           B        -1.2425                             
+    29 cc           B       -0.53125                             
+    30 ff           B       -0.78125                             
+    31 gg           B       -0.67125                             
+    32 jj           B       -0.60125                             
+    33 kk           B       -0.64625                             
+    34 ll           B       -0.67625                             
+    35 mm           B       -0.73125                             
+    36 nn           B       -0.56125                             
+    37 oo           B       -0.62125                             
+    38 pp           B       -0.63125                             
+    39 qq           B       -0.66125                             
+    40 rr           B       -0.70125                             
+    41 ss           B       -0.71125                             
+    42 tt           B       -0.79125                             
+    43 uu           B       -0.80125                             
+    44 maxError     B          0.025                             
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 1.78e-15 on row 56
+        max.rel.err = 3.43e-16 on row 56
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 1.50e-15 on column 8
+        max.rel.err = 8.88e-16 on column 7
+        High quality
+
+KKT.DB: max.abs.err = 4.44e-16 on row 12
+        max.rel.err = 4.44e-16 on row 12
+        High quality
+
+End of output
